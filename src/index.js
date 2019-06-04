d3bbc5701b54648205b36b418006cf490d8630ba
Toropon <duck7quick@gmail.com>
2019-06-04 16:30:37 +0900

    ゲーム履歴の保存(タイムトラベル)機能の追加

--- src/index.js
+++ src/index.js
@@ -12,55 +12,20 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  // Board にコンストラクタを追加し、初期 state に 9 個の null が 9 個のマス目に対応する 9 個の null 値をセット
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      // デフォルトで先手をバツに設定する
-      xIsNext: true,
-    };
-  }
-
-  // handleClickを定義する
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    // ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return するようにする
-    if (calculateWiunner(squares) || squares[i]) {
-      return;
-    }
-    
-    squares[i] = this.state.xIsNext ? '❌' : '⭕️';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   // Square に現在の値（'X'、'O' または null）を伝えるようにする
   // Board から Square に関数を渡すことにして、マス目がクリックされた時に Square にその関数を呼んでもらうようにする
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    // いずれかのプレーヤが勝利したかどうか判定。決着がついた場合は “Winner: X” あるいは “Winner: O”
-    const winner = calculateWiunner(this.state.squares);
-    let status;
-    if (winner) {
-      status ='Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕️');
-    }
-
     return(
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -82,14 +47,65 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // タイムトラベル機能の追加
+  // Game コンポーネントの初期 state をコンストラクタ内でセット
+  // 初期 state に 9 個の null が 9 個のマス目に対応する 9 個の null 値をセット
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      // デフォルトで先手をバツに設定する
+      xIsNext: true,
+    };
+  }
+
+  // handleClickを定義する
+  handleClick(i) {
+    // Game 内の handleClick メソッドで、新しい履歴エントリを history に追加
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    // ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return するようにする
+    if (calculateWiunner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? '❌' : '⭕️';
+    this.setState({
+      // concat()は元の配列をミューテートしない(コピーする)ためこっちを使う
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    // render 関数を更新して、ゲームのステータステキストの決定や表示の際に最新の履歴が使われるようにする
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    // いずれかのプレーヤが勝利したかどうか判定。決着がついた場合は “Winner: X” あるいは “Winner: O”
+    const winner = calculateWiunner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? '❌' : '⭕️');
+    }
+
     return(
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{ status }</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
